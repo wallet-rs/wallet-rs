556fb4ca4120488df1bc1f3899a5f07ccc580a1c
Shun Kakinoki <shunkakinoki@gmail.com>
2023-04-14 19:17:26 -0700

    feat: ini rust core w/ uniffi

---
+++ tools/mpl-license-checker/src/main.rs
@@ -0,0 +1,31 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use glob::glob;
+use std::fs;
+
+fn main() {
+    let dirs = ["crates/**/src/**/*.rs", "tools/**/src/**/*.rs"];
+    let mut failed = false;
+    for dir in dirs.iter() {
+        glob(dir)
+            .expect("Failed to read glob pattern")
+            .for_each(|entry| {
+                if let Ok(path) = entry {
+                    let contents = fs::read_to_string(&path).expect("Failed to read file");
+                    if !contents.starts_with(
+                        "// This Source Code Form is subject to the terms of the Mozilla Public\n\
+                     // License, v. 2.0. If a copy of the MPL was not distributed with this\n\
+                     // file, You can obtain one at https://mozilla.org/MPL/2.0/.\n",
+                    ) {
+                        println!("License header not found in file: {:?}", path);
+                        failed = true;
+                    }
+                }
+            });
+    }
+    if failed {
+        panic!("License header check failed");
+    }
+}
