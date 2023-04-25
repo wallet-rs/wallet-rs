5b50dc772791c7ea712c0d63ac10b02397833f07
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-04-24 20:21:40 -0700

    chore: wallet-rs-cli (#18)
    * chore: wallet-rs-cli
    
    * chore: update

--- bin/cli/src/main.rs
+++ bin/cli/src/main.rs
@@ -8,7 +8,7 @@
 /// From:
 /// https://github.com/paradigmxyz/reth/blob/df6ff63806cc6d3aa168278514b8d854f771d4b6/bin/reth/src/main.rs
 async fn main() {
-    if let Err(err) = wallet_cli::cli::run().await {
+    if let Err(err) = wallet_rs_cli::cli::run().await {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
