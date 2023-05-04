27c8c97fdc3e9e45d71aaa4488a5bc98bda74a13
Shun Kakinoki <shunkakinoki@gmail.com>
2023-05-04 13:56:21 -0700

    perf: ini commit for ios app

--- ios/WalletCore/WalletCoreApp.swift
+++ ios/WalletCore/WalletCoreApp.swift
@@ -4,14 +4,17 @@
 //
 //  Created by Shun Kakinoki on 5/4/23.
 //
-
+import Generated
 import SwiftUI
 
 @main
 struct WalletCoreApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        Button("Print") {
+            print(rustGreeting(name: "Bob"))
         }
     }
+  }
 }
--- ios/WalletCoreSource/Package.swift
+++ ios/WalletCoreSource/Package.swift
@@ -4,25 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "WalletCoreSource",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "WalletCoreSource",
-            targets: ["WalletCoreSource"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "WalletCoreSource",
-            dependencies: []),
-        .testTarget(
-            name: "WalletCoreSourceTests",
-            dependencies: ["WalletCoreSource"]),
-    ]
+  name: "WalletCoreSource",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "WalletCoreSource",
+      targets: ["WalletCoreSource"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .binaryTarget(name: "WalletCoreFFI", path: "../WalletCoreFFI.xcframework"),
+    .target(
+      name: "Generated",
+      dependencies: [
+        "WalletCoreFFI"
+      ]
+    ),
+    .target(
+      name: "WalletCoreSource",
+      dependencies: ["Generated"]),
+    .testTarget(
+      name: "WalletCoreSourceTests",
+      dependencies: ["WalletCoreSource"]),
+  ]
 )
