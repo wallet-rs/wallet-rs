7e2059372c8f402bee1a54d50b3ea95dab6b9d9f
Shun Kakinoki <shunkakinoki@gmail.com>
2023-05-04 12:48:23 -0700

    feat: ini ios project

---
+++ ios/wallet-rs/WalletRsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletRsApp.swift
+//  wallet-rs
+//
+//  Created by Shun Kakinoki on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WalletRsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
