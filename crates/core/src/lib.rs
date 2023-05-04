940959c85c8a52384f8dd5bf9273f769d752eef7
Shun Kakinoki <shunkakinoki@gmail.com>
2023-05-04 15:56:39 -0700

    perf: ini set keychain

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -2,11 +2,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#[cfg(target_os = "ios")]
+#[cfg(target_os = "linux")]
+use in_memory::InMemoryKeychain;
+#[cfg(any(target_os = "linux", target_os = "macos", target_os = "ios"))]
 use std::sync::Arc;
 #[cfg(target_os = "ios")]
 use wallet_keychain::ios::IOSKeychain;
-#[cfg(target_os = "ios")]
+#[cfg(target_os = "macos")]
+use wallet_keychain::macos::MacOSKeychain;
+#[cfg(any(target_os = "linux", target_os = "macos", target_os = "ios"))]
 use wallet_keychain::KeychainImpl;
 
 pub fn rust_greeting(to: String) -> String {
@@ -16,6 +20,42 @@ pub fn rust_greeting(to: String) -> String {
 #[cfg(target_os = "ios")]
 pub fn set_keychain(to: String) -> String {
     let keychain = Arc::new(IOSKeychain::new());
+    #[allow(clippy::redundant_clone)]
+    let _keychain2 = keychain.clone();
+
+    // Test that we can set and get a value.
+    _keychain2.set("test", &to).unwrap();
+
+    format!("Hello World, {}!", to)
+}
+
+#[cfg(target_os = "ios")]
+pub fn set_keychain(to: String) -> String {
+    let keychain = Arc::new(IOSKeychain::new());
+    #[allow(clippy::redundant_clone)]
+    let _keychain2 = keychain.clone();
+
+    // Test that we can set and get a value.
+    _keychain2.set("test", &to).unwrap();
+
+    format!("Hello World, {}!", to)
+}
+
+#[cfg(target_os = "linux")]
+pub fn set_keychain(to: String) -> String {
+    let keychain = Arc::new(InMemoryKeychain::new());
+    #[allow(clippy::redundant_clone)]
+    let _keychain2 = keychain.clone();
+
+    // Test that we can set and get a value.
+    _keychain2.set("test", &to).unwrap();
+
+    format!("Hello World, {}!", to)
+}
+
+#[cfg(target_os = "macos")]
+pub fn set_keychain(to: String) -> String {
+    let keychain = Arc::new(MacOSKeychain::new());
     #[allow(clippy::redundant_clone)]
     let _keychain2 = keychain.clone();
 
