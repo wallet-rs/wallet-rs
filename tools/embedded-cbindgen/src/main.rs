23aa1100fc9f01fc35d8d89dc5b335b80289cc54
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-19 14:04:45 -0700

    chore: update

--- tools/embedded-cbindgen/src/main.rs
+++ tools/embedded-cbindgen/src/main.rs
@@ -5,25 +5,26 @@
 use std::env;
 
 fn main() {
+    // Get the current directory
     let current_dir = env::current_dir().unwrap();
 
     // Get the command line arguments
     let args: Vec<String> = env::args().collect();
 
     // Find the index of the --output argument
-    let output_dir_index = args.iter().position(|arg| arg == "--output");
+    let output_index = args.iter().position(|arg| arg == "--output");
 
-    // Get the value of the --output-dir argument if it is present
-    let output_dir = output_dir_index.and_then(|i| args.get(i + 1));
+    // Get the value of the --output argument if it is present
+    let output = output_index.and_then(|i| args.get(i + 1));
 
     // Generate the C header file using the specified output directory, or the current directory if
     // no output directory is specified
-    if let Some(output_dir) = output_dir {
+    if let Some(output) = output {
         let _ = cbindgen::Builder::new()
             .with_crate(current_dir)
             .generate()
             .unwrap()
-            .write_to_file(output_dir);
+            .write_to_file(output);
     } else {
         let _ = cbindgen::generate(current_dir);
     }
