2778afa9ca2678b5396873fd6d437eb4f03ad817
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2023-06-21 16:14:27 -0700

    feat: feat/ini tauri (#64)
    * feat: ini pr validate
    
    * feat: ini tauri
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * feat: working expo
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: ini turbo token
    
    * chore: ini expo build
    
    * chore: ini node build
    
    * chore: ini node build
    
    ---------
    
    Signed-off-by: Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,18 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import { useColorScheme } from 'react-native'
 
 export default function HomeLayout() {
-  const [loaded] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
-  })
   const scheme = useColorScheme()
 
-  if (!loaded) {
-    return null
-  }
   return (
       <ThemeProvider value={scheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack />
--- apps/expo/tamagui.config.ts
+++
@@ -1,5 +0,0 @@
-// don't import from here, that's handled already
-// instead this is just setting types for this folder
-
-declare module 'tamagui' {}
-
