63a1d023cd134c2dc6d6a9ec440b6bea3198e17f
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-21 15:42:32 -0700

    chore: ini expo build

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,1 +1,11 @@
-import 'expo-router/entry'
+import { registerRootComponent } from "expo";
+import { ExpoRoot } from "expo-router";
+
+// From: https://expo.github.io/router/docs/troubleshooting/
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context("./app");
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
