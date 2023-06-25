d5e2ffd6ed788e0db227d434b7f57cf054187a15
Shun Kakinoki <shunkakinoki@gmail.com>
2023-06-25 15:39:01 -0700

    chore: ini nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,58 @@
 {
-  description = "A very basic flake";
+  inputs = {
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
 
-  outputs = { self, nixpkgs }: {
+      # The main branch follows the "canary" channel of the Android SDK
+      # repository. Use another android-nixpkgs branch to explicitly
+      # track an SDK release channel.
+      #
+      # url = "github:tadfisher/android-nixpkgs/stable";
+      # url = "github:tadfisher/android-nixpkgs/beta";
+      # url = "github:tadfisher/android-nixpkgs/preview";
+      # url = "github:tadfisher/android-nixpkgs/canary";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      # If you have nixpkgs as an input, this will replace the "nixpkgs" input
+      # for the "android" flake.
+      #
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/master";
+    };
   };
+
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    android-nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      android-stable = pkgs.callPackage android-nixpkgs {
+        channel = "stable";
+      };
+      android-sdk.packages = sdkPkgs: with sdkPkgs; [
+        build-tools-31-0-0
+        cmdline-tools-latest
+        emulator
+        platforms-android-31
+        sources-android-31
+      ];
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs
+          pkgs.nodejs-18_x
+          pkgs.nodePackages.pnpm
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.typescript-language-server
+        ];
+      };
+    });
 }
